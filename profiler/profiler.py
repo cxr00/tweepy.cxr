26fb4ed1acf6616060deaf7f97177cb08d1e47e3
Joshua Conrad <jozbornn@gmail.com>
2022-08-25 14:12:19 -0700

    Coming soon!

---
+++ profiler/profiler.py
@@ -0,0 +1,14 @@
+
+from tkinter import Tk, ttk, Text, StringVar, messagebox
+import tkinter as tk
+import tweepy as tw
+
+
+class Profiler:
+
+    def __init__(self):
+        super().__init__()
+
+    def get_user_profile(self, id=None, username=None):
+        if not (id or username):
+            raise ValueError(f"Please specify either the Twitter ID or Username of the user you would like to profile.")
